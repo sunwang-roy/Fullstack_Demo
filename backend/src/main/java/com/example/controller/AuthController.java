757141372c5cc5fd4dbf40a6097a22446501e7ae
sunwang-roy <sunwang0702@163.com>
2026-01-14 15:58:37 +0800

    Create AuthController.java

---
+++ backend/src/main/java/com/example/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.example.controller;
+
+import com.example.dto.ApiResponse;
+import com.example.dto.LoginRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    // 固定的验证码
+    private static final String FIXED_CODE = "123456";
+
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<String>> login(@Valid @RequestBody LoginRequest loginRequest) {
+        if (getVerifyCode().equals(loginRequest.getVerificationCode())) {
+            // in real env, should return a JWT Token
+            return ResponseEntity.ok(new ApiResponse<>(true, "Succesful!", loginRequest.getPhoneNumber()));
+        } else {
+            return ResponseEntity.badRequest().body(new ApiResponse<>(false, "Failed"));
+        }
+    }
+
+  // in real env, we get it from Redis or Kafka
+   public String getVerifyCode() {
+     return FIXED_CODE;
+}
